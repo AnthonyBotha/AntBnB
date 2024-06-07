92c2a0a82dd30a230eeac7bbb2af2c154bca2c92
AnthonyBotha <anthony.botha@outlook.com>
2024-06-06 19:30:48 -0700

    Added options object to migrations

--- backend/db/migrations/02-create-spot.js
+++ backend/db/migrations/02-create-spot.js
@@ -1,4 +1,12 @@
 'use strict';
+
+let options = {};
+options.tableName = "Spots";
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -63,6 +71,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Spots');
+    options.tableName = "Spots";
+    await queryInterface.dropTable(options);
   }
 };
--- backend/db/migrations/06-create-review-image.js
+++ backend/db/migrations/06-create-review-image.js
@@ -1,4 +1,12 @@
 'use strict';
+
+let options = {};
+options.tableName = "ReviewImages";
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -31,6 +39,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('ReviewImages');
+    options.tableName = "ReviewImages";
+    await queryInterface.dropTable(options);
   }
 };
